be2b997f594a2a0cd301d85465a4eb208c4cf6df
Kumarsaieerla <117519958+Kumarsaieerla@users.noreply.github.com>
2024-12-07 22:50:13 +0530

    updates

--- start_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-docker pull saieerla/simple-python-app:latest
-docker run -d -p 5000:5000 saieerla/simple-python-app:latest
-echo "sucessfullly docker image running"
--- stop_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-containerid=`docker ps | awk -F " " '{print}'`
-docker rm -f $containerid
