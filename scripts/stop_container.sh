f1b9a54e865a39a2534cea67f0135827e726ae36
Kumarsaieerla <117519958+Kumarsaieerla@users.noreply.github.com>
2024-12-07 22:58:37 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "hai"
-# set -e
-# containerid=`docker ps | awk -F " " '{print}'`
-# docker rm -f $containerid
+set -e
+containerid=`docker ps | awk -F " " '{print}'`
+docker rm -f $containerid
 .
